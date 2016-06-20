51066c7117be05a658eac355732e056498cd5648
Vojta Podhajsky <Lameorc@gmail.com>
2016-06-20 16:54:55 +0200

    Added HTML/CSS files to project. Fixed html to be valid in thymeleaf

---
+++ java/src/main/java/webapp/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Vojta Podhajsky on 20.06.2016.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+
+    }
+}
