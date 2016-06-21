2b5f924bac68d4069bcb0b7087f75a49911647eb
Vojta Podhajsky <Lameorc@gmail.com>
2016-06-21 14:52:41 +0200

    Login now works, but on first login, it redirects to either css or js, Don't know the solution to this

--- java/src/main/java/webapp/config/WebSecurityConfig.java
+++ java/src/main/java/webapp/config/WebSecurityConfig.java
@@ -4,31 +4,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource ds;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/index","/css/**","/fonts/**", "/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
+                .defaultSuccessUrl("/")
                 .loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
+
     }
+
+    @Override
+    public void configure(WebSecurity security){
+        security.ignoring().antMatchers("/css/**","/fonts/**","/libs/**");
+    }
+
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+        auth.jdbcAuthentication().dataSource(ds);
     }
 }
--- java/src/main/java/webapp/controllers/indexController.java
+++ java/src/main/java/webapp/controllers/indexController.java
@@ -1,8 +1,15 @@
 package webapp.controllers;
 
+import backend.Managers.MenuManager;
+import backend.entities.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by Vojta Podhajsky on 18.06.2016.
@@ -10,8 +17,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class indexController {
 
+    @Autowired
+    private ApplicationContext context;
+
     @RequestMapping("/")
     public String index(Model model){
+        MenuManager menuManager = (MenuManager) context.getBean("menuManager");
+        List<Menu> menus = menuManager.getAllMenus();
+
+        menus.removeIf(m ->
+                !(m.getStartDate().isBefore(LocalDate.now())
+                        && m.getEndDate().isAfter(LocalDate.now())));
+        model.addAttribute("menus", menus);
         return "index";
     }
 }
