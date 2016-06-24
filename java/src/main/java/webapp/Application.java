8e9337ddff17a121b714acb30df9ad5f04266fe2
Vojta Podhajský <lameorc@gmail.com>
2016-06-24 11:27:12 +0200

    spring sec + thymeleaf

--- java/src/main/java/webapp/Application.java
+++ java/src/main/java/webapp/Application.java
@@ -1,8 +1,11 @@
 package webapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 /**
  * Created by Vojta Podhajsky on 18.06.2016.
@@ -14,4 +17,11 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+    @Autowired
+    private TemplateResolver templateResolver;
+    
+    @Bean
+    public TemplateResolver templateResolver(){
+        return templateResolver;
+    };
 }
